467292deaa9b4b5ef55f2f9a39ae64b521020bf6
AntonFly <Vivaeltoro99>
2018-11-11 15:18:11 +0300

    startversion1

---
+++ src/main/java/ConnectionData.java
@@ -0,0 +1,49 @@
+import javax.persistence.*;
+
+@Entity
+@Table(name ="connectiondata")
+public class ConnectionData {
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getMobilenumb() {
+        return mobilenumb;
+    }
+
+    public void setMobilenumb(int mobilenumb) {
+        this.mobilenumb = mobilenumb;
+    }
+
+    @Id
+    @Column(name= "login")
+    @OneToOne(mappedBy = "connectiondata")
+    private String login;
+    @Column(name = "E-mail")
+    private String email;
+    @Column(name="mobilenumb")
+    private int mobilenumb;
+
+
+    public ConnectionData(String login, String email, int mobilenumb) {
+        this.login = login;
+        this.email = email;
+        this.mobilenumb = mobilenumb;
+    }
+
+    public ConnectionData() {
+    }
+}
+
