24bbf775d1ee12507dacb9369ef28b32c23d8aad
AntonFly <anton99910@gmail.com>
2018-12-08 18:36:53 +0300

    Merge branch 'anton' of https://github.com/AntonFly/MultHub into anton
    # Conflicts:
    #	.idea/dataSources/9c81a433-c969-4d22-ba65-9bc2b7df0379.xml
    #	.idea/dataSources/9c81a433-c969-4d22-ba65-9bc2b7df0379/storage_v2/_src_/database/multhub.xXXWUw/schema/public.abK9xQ.meta
    #	src/main/java/entity/CommentsEntity.java
    #	src/test/java/ProjectsServiceTests.java

--- src/test/java/ProjectsServiceTests.java
+++ src/test/java/ProjectsServiceTests.java
@@ -1,52 +1,87 @@
 import dao.DaoFactory;
 import entity.*;
 import exception.DBException;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.jupiter.api.*;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
+import org.junit.runners.MethodSorters;
 import service.ProjectService;
 import service.UserService;
 
 import java.math.BigInteger;
 import java.sql.Timestamp;
+import java.util.UUID;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ProjectsServiceTests {
     private ProjectService ps;
-    String login = "3d";
-    String newPassword = "updated";
+    static ProjectsEntity pe;
 
     static void main(String[] args) {
         JUnitCore runner = new JUnitCore();
-        Result result = runner.run(UsersEntityTests.class);
-        System.out.println("run tests: " + result.getRunCount());
-        System.out.println("failed tests: " + result.getFailureCount());
-        System.out.println("ignored tests: " + result.getIgnoreCount());
-        System.out.println("success: " + result.wasSuccessful());
+        Result result = runner.run(ProjectsServiceTests.class);
     }
 
     @BeforeAll
     void init() {
         ps = new ProjectService();
-    }
-
-    @Test
-    void addProject() {
-        ProjectsEntity pe = new ProjectsEntity();
+        pe = new ProjectsEntity();
         pe.setProjectid(null);
         pe.setCurbudget(12.);
         pe.setDescription("V 1999 GODU rodilsa divan i vosstal");
         pe.setGoalbudget(13.);
         pe.setName("Vosstanie mashine");
+    }
+
+
+    @Test
+    void addProject() {
         try {
             ps.create(pe);
-            System.out.println(pe.getProjectid());
-            System.out.println(ps.get(pe.getProjectid()).getName());
         } catch (DBException e) {
             e.printStackTrace();
             Assertions.fail("Ошибка добавления объекта");
         }
+    }
 
+//    @Test
+//    void deleteProject(){
+//
+//        try {
+//            ps.delete(UUID.nameUUIDFromBytes((pe.getName()+pe.getDescription()).getBytes()).toString());
+//
+//        } catch (DBException e) {
+//            e.printStackTrace();
+//            Assertions.fail("Ошибка добавления объекта");
+//        }
+//    }
+
+//    @Test
+//    void updateProject(){
+//        pe.setName("LOL CHANGED");
+//        try {
+//            ps.update(pe);
+//            System.out.println(pe.getProjectid());
+//            System.out.println(ps.get(pe.getProjectid()).getName());
+//        } catch (DBException e) {
+//            e.printStackTrace();
+//            Assertions.fail("Ошибка добавления объекта");
+//        }
+//
+//    }
+
+    @Test
+    void getProject() {
+        try {
+            System.out.println("UPDATED:" +ps.get(UUID.nameUUIDFromBytes((pe.getName()+pe.getDescription()).getBytes()).toString()).getName());
+
+        } catch (DBException e) {
+            e.printStackTrace();
+            Assertions.fail("Ошибка добавления объекта");
+        }
     }
 
 }
